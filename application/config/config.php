d43b564eb0e1831c0dacb2abb819339f676c5ba6
gabrielnunez1 <32506651+gabrielnunez1@users.noreply.github.com>
2019-02-15 10:27:08 -0300

    Cambios

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/TP2K16';
+$config['base_url'] = 'http://localhost/ajaxSelectList';
 
 /*
 |--------------------------------------------------------------------------
